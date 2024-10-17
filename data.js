2f2e970e3c6631080751b8348e53acc85852dc5f
sachin <sachinsharma11175@gmail.com>
2024-10-17 21:42:14 +0530

    Add round data to data.js

---
+++ data.js
@@ -0,0 +1,20 @@
+const roundData = {
+    1: {
+        A: "https://link-to-section-a-year-1",
+        B: "https://link-to-section-b-year-1",
+    },
+    2: {
+        A: [
+            { name: "Student 1", link: "https://form-link-1" },
+            { name: "Student 2", link: "https://form-link-2" },
+        ],
+        B: "https://contest-link-year-2",
+    },
+    3: {
+        A: [
+            { name: "Student 1", link: "https://form-link-1" },
+            { name: "Student 2", link: "https://form-link-2" },
+        ],
+        B: "https://contest-link-year-3",
+    },
+};
